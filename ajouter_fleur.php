793ff52ec1d5d5e8497176390a0fd8f3715df36c
Elodie Bouzekri <elodie.bouzekri@alumni.enac.fr>
2017-02-07 12:03:29 +0100

    HTML + PHP ajouter fleur

---
+++ ajouter_fleur.php
@@ -0,0 +1,12 @@
+<?php
+if(!empty($_POST)){
+   
+    $nom = $_POST['nom'];
+    $date = $_POST['date'];
+    $couleur = $_POST['couleur'];
+    $prix = $_POST['prix'];
+    $image = $_POST['image'];
+    $variete = $_POST['variete'];
+    echo $_POST;
+}
+?>
